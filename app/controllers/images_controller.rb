fd7356d9e81cde1a64017bcab827188984e31e46
Meaghan Jones and Sam Park <sam@>
2016-09-14 16:26:08 -0700

    add view to view all images a user has uploaded

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,10 @@
 class ImagesController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    @images = Image.all
+  end
+
   def show
     @image = Image.find(params[:id])
   end
