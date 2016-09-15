e66dc3fad9fae37acf34077e114d7f9cea7b6adb
Meaghan Jones and Sam Park <meaghan+sam@>
2016-09-15 10:55:11 -0700

    add RUD to images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -24,6 +24,27 @@ class ImagesController < ApplicationController
     end
   end
 
+  def edit
+      @image = Image.find(params[:id])
+    end
+
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      flash[:notice] = "Image successfully updated"
+      redirect_to images_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to user_images_path
+  end
+
+
 private
   def image_params
     params.require(:image).permit(:photo, :tag, :title)
